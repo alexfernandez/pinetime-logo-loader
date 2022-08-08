046f4cded1887142b41dc7860c3028a9d2a675ec
Alex Fernandez <alexfernandeznpm@gmail.com>
2022-08-08 21:50:40 +0200

    Revert "Update mynewt version"
    This reverts commit 1663e522da6f8979202d266b0a8b93c607cf8412.

--- scripts/install-version.sh
+++ scripts/install-version.sh
@@ -2,7 +2,7 @@
 #  Versions of Mynewt and NimBLE to install on all platforms
 
 #  Versions to install
-mynewt_version=mynewt_1_10_0_tag
+mynewt_version=mynewt_1_7_0_tag
 nimble_version=nimble_1_2_0_tag
 mcuboot_version=v1.5.0
 openocd_version=master
